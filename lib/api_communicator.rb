8248d1027091304a7521b6b01c8934e47346c80b
nkalkstein <nkalkstein@gmail.com>
2018-02-21 13:49:49 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,22 +4,46 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
+  charfilms = nil
+  filmList = []
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+
+  character_hash["results"].each { |oneCharacter|
+    thisChar = oneCharacter["name"].downcase
+    if thisChar.include?(character)
+      charfilms= oneCharacter["films"]
+    end
+  }
+
+
+  if charfilms
+    charfilms.each { |filmApi|
+      allFilmData = RestClient.get(filmApi)
+      film_hash = JSON.parse(allFilmData)
+      # puts film_hash["title"]
+      filmList.push(film_hash)
+    }
+  end
+
+  filmList
 end
 
+# iterate over the character hash to find the collection of `films` for the given
+#   `character`
+# collect those film API urls, make a web request to each URL to get the info
+#  for that film
+# return value of this method should be collection of info about each film.
+#  i.e. an array of hashes in which each hash reps a given film
+# this collection will be the argument given to `parse_character_movies`
+#  and that method will do some nice presentation stuff: puts out a list
+#  of movies by title. play around with puts out other info about a given film.
+
+
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.each { |film|
+    puts film["title"]
+  }
 end
 
 def show_character_movies(character)
