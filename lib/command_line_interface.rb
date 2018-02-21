e50410056d4069dd4c1d1e2278371d4461971b6b
github username <github email address>
2018-02-21 14:42:53 -0500

    Created star wars methods

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 def welcome
-  # puts out a welcome message here!
+  puts "Hello!"
 end
 
 def get_character_from_user
   puts "please enter a character"
+  gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
