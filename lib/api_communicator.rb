2838435cb75a16b849188b3cedeb7c449dd93142
dannyb21892 <dannyb21892@verizon.net>
2018-02-21 12:03:25 -0500

    Done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,25 +1,52 @@
-require 'rest-client'
-require 'json'
-require 'pry'
+require "rest-client"
+require "json"
 
 def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+
+  page = 1
+  stop = 0
+  out = []
+
+  while stop == 0
+
+    all_characters = RestClient.get("http://www.swapi.co/api/people/?page=#{page}")
+    character_hash = JSON.parse(all_characters)
+
+    if character_hash["next"] == nil
+      stop = 1
+    end
+
+    character_hash["results"].each do |chars|
+
+      if chars["name"] == character
+        movies = chars["films"]
+
+        movies.each do |movieurl|
+          film = all_characters = RestClient.get("#{movieurl}")
+          film_hash = JSON.parse(film)
+          out << film_hash
+        end
+        return out
+
+      end
+    end
+    page += 1
+  end
+  puts "Sorry, character not found."
+  out
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+
+  if films_hash.length > 0
+    movienum = 1
+    films_hash.each do |film|
+      puts "#{movienum}. #{film["title"]}"
+      movienum += 1
+    end
+  else
+    puts "There are no movies."
+  end
 end
 
 def show_character_movies(character)
